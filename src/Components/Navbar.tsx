64d5ffb28c77d14b3b85dfc73e89344b205e1658
Divin <divinlion80@gmail.com>
2023-12-21 20:47:29 +0530

    first commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-white p-6">
+    {/* Your content here */}
+  </nav>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Link from "next/link";
+import Navbar from "~/Components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
