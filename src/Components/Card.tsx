61b00ae7b1ab795d80e25b728dbdc05daef082f7
Divin <divinlion80@gmail.com>
2023-12-22 07:44:07 +0530

    grouping according to title priority status

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
   const [users, setUsers] = useState([]);
+  const [groupBy, setGroupBy] = useState('status'); // Default grouping by status
+  const [groupedTasks, setGroupedTasks] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,30 +23,101 @@ const TaskList = () => {
     fetchData();
   }, []);
 
-  // Group tasks by status
-  const groupedTasks = tasks.reduce((acc, task) => {
-    if (!acc[task.status]) {
-      acc[task.status] = [];
+  // Function to group tasks based on selected criteria (status, user, priority)
+  useEffect(() => {
+    const grouped = tasks.reduce((acc, task) => {
+      const key = task[groupBy];
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(task);
+      return acc;
+    }, {});
+    setGroupedTasks(grouped);
+  }, [tasks, groupBy]);
+
+  // Function to sort tasks by title or priority across all user groups
+  const sortTasks = (sortKey) => {
+    if (groupBy === 'userId'||groupBy === 'priority'||groupBy === 'status') {
+      const updatedGroupedTasks = {};
+      Object.keys(groupedTasks).forEach(key => {
+        updatedGroupedTasks[key] = groupedTasks[key].sort((a, b) => {
+          if (sortKey === 'title') {
+            return a.title.localeCompare(b.title);
+          } else if (sortKey === 'priority') {
+            return b.priority - a.priority;
+          }
+          return 0;
+        });
+      });
+      setGroupedTasks(updatedGroupedTasks);
     }
-    acc[task.status].push(task);
-    return acc;
-  }, {});
+  };
 
   return (
     <div className="flex justify-center">
-      {Object.entries(groupedTasks).map(([status, tasksForStatus]) => (
-        <div key={status} className="p-4">
-          <h2 className="text-lg font-semibold mb-4">{status}</h2>
-          {tasksForStatus.map(task => (
-            <div key={task.id} className="max-w-xs bg-white shadow-md rounded-md p-4 mb-4">
-              <h3 className="font-semibold text-lg mb-2">{task.title}</h3>
-              <p className="text-gray-700">Status: {task.status}</p>
-              <p className="text-gray-700">Priority: {task.priority}</p>
-              <p className="text-gray-700">Assigned To: {users.find(user => user.id === task.userId)?.name || 'Unassigned'}</p>
+      <div className="flex flex-col items-center">
+        <div className="mb-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setGroupBy('status')}>
+            Group by Status
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2" onClick={() => setGroupBy('userId')}>
+            Group by User
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setGroupBy('priority')}>
+            Group by Priority
+          </button>
+        </div>
+
+        {groupBy === 'userId' && (
+          <div className="mb-4">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2" onClick={() => sortTasks('title')}>
+              Sort by Title
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={() => sortTasks('priority')}>
+              Sort by Priority
+            </button>
+          </div>
+        )}
+
+        {groupBy === 'priority' && (
+          <div className="mb-4">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2" onClick={() => sortTasks('title')}>
+              Sort by Title
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={() => sortTasks('priority')}>
+              Sort by Priority
+            </button>
+          </div>
+        )} 
+
+         {groupBy === 'status' && (
+          <div className="mb-4">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2" onClick={() => sortTasks('title')}>
+              Sort by Title
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={() => sortTasks('priority')}>
+              Sort by Priority
+            </button>
+          </div>
+        )} 
+
+        <div className="grid grid-cols-3 gap-4">
+          {Object.entries(groupedTasks).map(([key, tasksForGroup]) => (
+            <div key={key}>
+              <h2 className="text-lg font-semibold mb-4">{key}</h2>
+              {tasksForGroup.map(task => (
+                <div key={task.id} className="max-w-xs bg-white shadow-md rounded-md p-4 mb-4">
+                  <h3 className="font-semibold text-lg mb-2">{task.title}</h3>
+                  <p className="text-gray-700">Status: {task.status}</p>
+                  <p className="text-gray-700">Priority: {task.priority}</p>
+                  <p className="text-gray-700">Assigned To: {users.find(user => user.id === task.userId)?.name || 'Unassigned'}</p>
+                </div>
+              ))}
             </div>
           ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
