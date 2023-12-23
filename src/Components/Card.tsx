85388ca04ca315f6cc6b8733b8ff431ce1c79007
Divin <divinlion80@gmail.com>
2023-12-23 11:07:31 +0530

    fixing error

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,3 @@
-// TaskList.js
-
 import React from 'react';
 import { useTaskContext } from './Context';
 
@@ -10,6 +8,15 @@ interface Task {
   status: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined;
   priority: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined;
   userId: any;
+}
+
+interface GroupedTasks {
+  [key: string]: Task[]; // Define the structure of groupedTasks
+}
+
+interface User {
+  id: number;
+  name: string;
 }
 
 const TaskList = () => {
@@ -23,21 +30,24 @@ const TaskList = () => {
     setSortBy,
     sortTasks,
   } = useTaskContext();
-  
+
   // Assuming tasksForGroup is an array of Task
-  const tasksForGroup: Task[] = tasks || []; 
+  const tasksForGroup: Task[] = tasks || [];
+
+  // Explicitly define the type of groupedTasks
+  const typedGroupedTasks: GroupedTasks = groupedTasks || {};
 
   // Function to render task items based on the groupedTasks
   const renderTasks = () => {
-    return Object.entries(groupedTasks).map(([key, tasksForGroup]) => (
+    return Object.entries(typedGroupedTasks).map(([key, groupedTaskItems]) => (
       <div key={key}>
         <h2 className="text-lg font-semibold mb-4">{key}</h2>
-        {tasksForGroup.map((task: Task) => (
+        {groupedTaskItems.map((task: Task) => (
           <div key={task.id} className="max-w-xs bg-white shadow-md rounded-md p-4 mb-4">
             <h3 className="font-semibold text-lg mb-2">{task.title}</h3>
             <p className="text-gray-700">Status: {task.status}</p>
             <p className="text-gray-700">Priority: {task.priority}</p>
-            <p className="text-gray-700">Assigned To: {users.find(user => user.id === task.userId)?.name || 'Unassigned'}</p>
+            <p className="text-gray-700">Assigned To: {users.find((user: User) => user.id === task.userId)?.name || 'Unassigned'}</p>
           </div>
         ))}
       </div>
@@ -48,14 +58,6 @@ const TaskList = () => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center">
-
-        {/* Buttons for sorting based on groupBy */}
-        {(groupBy === 'userId' || groupBy === 'priority' || groupBy === 'status') && (
-          <div className="mb-4">
-            {/* Your buttons here */}
-          </div>
-        )}
-
         {/* Rendering tasks */}
         <div className="grid grid-cols-3 gap-4">
           {renderTasks()}
